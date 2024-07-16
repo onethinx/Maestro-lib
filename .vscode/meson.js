f9f1b0c84c58475f130c7c801fd38ac0a723e4ec
RolfNoot <rolf@onethinx.com>
2024-07-16 13:00:27 +0200

    Added loading of update.json

--- .vscode/meson.js
+++ .vscode/meson.js
@@ -141,6 +141,7 @@ otxUpdate = async (args) => {
 	try
 	{
 		updateFile(".vscode", 'project.json');
+		updateFile(".vscode", 'update.json');
 	}
 	catch {}
 	updateFile(".vscode", 'c_cpp_properties.json');
