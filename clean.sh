26a2d6fe107ab5215bc67c0c5cce4f13a571a521
RolfNoot <rolf@onethinx.com>
2025-04-05 20:13:23 +0200

    SideBar update for OTX Maestro 1.1.0

--- clean.sh
+++ clean.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# Get the files
-SCRIPT_DIR="$(dirname "$0")"
-MESON_FILE="$SCRIPT_DIR/meson.build"
-BRD_CFG_FILE="$SCRIPT_DIR/.vscode/brd.cfg"
+# Get the project directory (where the script is located)
+PROJECT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# Change to the project directory before running functions
+cd "$PROJECT_DIR"
+
+# Define file paths relative to the project directory
+MESON_FILE="./meson.build"
+BRD_CFG_FILE="./.vscode/brd.cfg"
 
 ## Function to remove everything between #***_Start and #***_End in meson.build
 clean_meson_file() {
+  echo "Cleaning $(realpath "$MESON_FILE")"
   awk '
   BEGIN { keep=1 }
   /#.*_Start/ { print; keep=0 }
@@ -15,11 +21,17 @@ clean_meson_file() {
   ' "$MESON_FILE" > "$MESON_FILE.tmp" && mv "$MESON_FILE.tmp" "$MESON_FILE"
   sed -i '' -e '/#.*_Line/{n;s/.*//;}' "$MESON_FILE"
   sed -i '' -e '/#.*_Lines/{n;s/.*//;n;s/.*//;}' "$MESON_FILE"
-  echo "Cleaned $MESON_FILE"
+}
+
+clean_build_folder() {
+  echo "Cleaning $(realpath "./build")"
+  find "./build" -type f ! -name '*.elf' ! -name '*.hex' -exec rm -f {} +
+  find "./build" -type d -mindepth 1 -exec rm -rf {} +
 }
 
 # Function to remove the first line of .vscode/brd.cfg if it contains "PROGRAMMER"
 clean_brd_cfg_file() {
+  echo "Cleaning $(realpath "$BRD_CFG_FILE")"
   # Read the first line of the file
   first_line=$(head -n 1 "$BRD_CFG_FILE")
   
@@ -28,9 +40,45 @@ clean_brd_cfg_file() {
     # Remove the first line
     sed -i '' -e '1d' "$BRD_CFG_FILE"
   fi
-  echo "Cleaned $BRD_CFG_FILE"
+}
+
+clean_psoccreator_folder() {
+  echo "Cleaning folders and files in $PWD"
+
+  # Remove directories and their contents
+  rm -rf codegentemp
+  rm -rf CortexM0
+  rm -rf CortexM0p
+  rm -rf CortexM3
+  rm -rf CortexM4
+  #rm -rf Generated_Source
+  #rm -rf Export
+  rm -rf armgcc
+  rm -rf iar
+  rm -rf mdk
+
+  # Remove specific file types
+  rm -f *.rpt
+  rm -f *.cyfit
+  rm -f *.cycdx
+  rm -f *.html
+  rm -f *.svd
+  rm -f *.scat
+  #rm -f *.ld
+  rm -f *.icf
+  rm -f *.cyprj.*
+  rm -f *.cywrk.*
+  #rm -f *system_psoc63_cm*.c
+  #rm -f cy_ipc_config.*
 }
 
 # Run the functions
 clean_meson_file
 clean_brd_cfg_file
+clean_build_folder
+
+# Execute cleaning function in every .cydsn folder in the immediate directory
+for dir in $(find "$PROJECT_DIR" -maxdepth 1 -type d -name "*.cydsn"); do
+   cd "$dir"
+   clean_psoccreator_folder;
+done
